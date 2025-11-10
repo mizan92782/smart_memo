df1441345cdbb838ff13ba5ac86e7b031a2ded86
mizanur rahman <mizanmd92782@gmail.com>
2025-11-10 18:30:33 +0600

    adding product view templates

--- categories/models.py
+++ categories/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 
@@ -12,3 +12,10 @@ class Category(models.Model):
 
   def __str__(self):
       return self.name
+  
+
+  #! its take a instance of category (by self) and return the url name with wtih neccessry data: in kwgs 
+  #! "url-name" defines templated will get this data: such that self.pk,self.name
+
+  def get_category_products_url(self):  # will take a instance
+     return reverse('category_products', kwargs={'pk': self.pk })
--- smart_memo/urls.py
+++ smart_memo/urls.py
@@ -17,8 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from products import views
+from smart_memo import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('products.urls')),
+    path('products/',include('products.urls')),
+    path('categories/',include('categories.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
